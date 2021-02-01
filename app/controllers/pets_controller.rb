8de2ab6020d6b9a241208d6ce7179434bb1668f6
stacy <stacydn23@gmail.com>
2021-02-01 10:14:42 -0500

    commit tree e59b4d52631ea476cb5cd147abb6126765a74e07

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,11 +6,18 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+     @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    # @owner=Owner.find_by(@params[:pet_owner_id])
+    # @pet = Pet.create(name:@params[:pet_name],owner:@owner)
+    @pet = Pet.create(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
@@ -19,8 +26,20 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do
+    # @pet=Pet.find_by_id(@params[:id])
+    @pet = Pet.find(params[:id])
+    # @owners=Owner.all
+    erb :'/pets/edit'
+  end
 
+  post '/pets/:id' do 
+    @pet = Pet.find(params[:id])
+    @pet.update(params["pet"])
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
